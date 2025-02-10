fd48dbde1b7e99b8b38efb5218c5614cd46a40e9
AKASH-1AK <akashsofficial03@gmail.com>
2025-02-10 22:02:00 +0530

    initial commit

---
+++ overall_query.sql
@@ -0,0 +1,167 @@
+CREATE TABLE city
+(
+	city_id	INT PRIMARY KEY,
+	city_name VARCHAR(15),	
+	population	BIGINT,
+	estimated_rent	FLOAT,
+	city_rank INT
+);
+
+CREATE TABLE customers
+(
+	customer_id INT PRIMARY KEY,	
+	customer_name VARCHAR(25),	
+	city_id INT,
+	CONSTRAINT fk_city FOREIGN KEY (city_id) REFERENCES city(city_id)
+);
+
+
+CREATE TABLE products
+(
+	product_id	INT PRIMARY KEY,
+	product_name VARCHAR(35),	
+	Price float
+);
+
+
+CREATE TABLE sales
+(
+	sale_id	INT PRIMARY KEY,
+	sale_date	date,
+	product_id	INT,
+	customer_id	INT,
+	total FLOAT,
+	rating INT,
+	CONSTRAINT fk_products FOREIGN KEY (product_id) REFERENCES products(product_id),
+	CONSTRAINT fk_customers FOREIGN KEY (customer_id) REFERENCES customers(customer_id) 
+);
+
+select * from city;
+
+
+select 
+	city_name,
+	ROUND(
+(population * 0.25)/1000000,2
+	) as coffe_consumer_in_millions,
+	city_rank
+from city
+order by 2 desc
+
+select
+	sum(total) as total_revenue
+from sales
+where
+	extract(year from sale_date) = 2023
+	and
+	extract(quarter from sale_date) = 4
+
+SELECT 
+	ci.city_name,
+	SUM(s.total) as total_revenue
+FROM sales as s
+JOIN customers as c
+ON s.customer_id = c.customer_id
+JOIN city as ci
+ON ci.city_id = c.city_id
+WHERE 
+	EXTRACT(YEAR FROM s.sale_date)  = 2023
+	AND
+	EXTRACT(quarter FROM s.sale_date) = 4
+GROUP BY 1
+ORDER BY 2 DESC
+
+SELECT 
+	p.product_name,
+	COUNT(s.sale_id) as total_orders
+FROM products as p
+LEFT JOIN
+sales as s
+ON s.product_id = p.product_id
+GROUP BY 1
+ORDER BY 2 DESC
+
+
+SELECT 
+	ci.city_name,
+	SUM(s.total) as total_revenue,
+	COUNT(DISTINCT s.customer_id) as total_cx,
+	ROUND(
+			SUM(s.total)::numeric/
+				COUNT(DISTINCT s.customer_id)::numeric
+			,2) as avg_sale_pr_cx
+	
+FROM sales as s
+JOIN customers as c
+ON s.customer_id = c.customer_id
+JOIN city as ci
+ON ci.city_id = c.city_id
+GROUP BY 1
+ORDER BY 2 DESC
+
+WITH city_table as 
+(
+	SELECT 
+		city_name,
+		ROUND((population * 0.25)/1000000, 2) as coffee_consumers
+	FROM city
+),
+customers_table
+AS
+(
+	SELECT 
+		ci.city_name,
+		COUNT(DISTINCT c.customer_id) as unique_cx
+	FROM sales as s
+	JOIN customers as c
+	ON c.customer_id = s.customer_id
+	JOIN city as ci
+	ON ci.city_id = c.city_id
+	GROUP BY 1
+)
+SELECT 
+	customers_table.city_name,
+	city_table.coffee_consumers as coffee_consumer_in_millions,
+	customers_table.unique_cx
+FROM city_table
+JOIN 
+customers_table
+ON city_table.city_name = customers_table.city_name
+
+SELECT * 
+FROM -- table
+(
+	SELECT 
+		ci.city_name,
+		p.product_name,
+		COUNT(s.sale_id) as total_orders,
+		DENSE_RANK() OVER(PARTITION BY ci.city_name ORDER BY COUNT(s.sale_id) DESC) as rank
+	FROM sales as s
+	JOIN products as p
+	ON s.product_id = p.product_id
+	JOIN customers as c
+	ON c.customer_id = s.customer_id
+	JOIN city as ci
+	ON ci.city_id = c.city_id
+	GROUP BY 1, 2
+	-- ORDER BY 1, 3 DESC
+) as t1
+WHERE rank <= 3
+
+SELECT * FROM products;
+
+
+
+SELECT 
+	ci.city_name,
+	COUNT(DISTINCT c.customer_id) as unique_cx
+FROM city as ci
+LEFT JOIN
+customers as c
+ON c.city_id = ci.city_id
+JOIN sales as s
+ON s.customer_id = c.customer_id
+WHERE 
+	s.product_id IN (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14)
+GROUP BY 1
+
